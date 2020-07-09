97c781dd64c622eb3f7e9c022db852fe79fd3a89
Raphael <raphaelndauwa@gmail.com>
2020-07-09 14:06:33 +0300

    add properties to ensure items are loaded

--- src/main/java/com/example/codebbq254freemarker/controller/StudentList.java
+++ src/main/java/com/example/codebbq254freemarker/controller/StudentList.java
@@ -4,6 +4,8 @@ import com.example.codebbq254freemarker.model.Student;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -11,15 +13,14 @@ import java.util.List;
 @Controller
 public class StudentList {
 
-    @RequestMapping("/list")
-
+    @RequestMapping(value = "/list", method = RequestMethod.GET)
     public String list(Model model) {
         List<Student> list = new ArrayList<>();
         list.add(new Student(1, "Tom", 80.5));
         list.add(new Student(2, "Jerry", 90.4));
         list.add(new Student(3, "Paul", 77.5));
         model.addAttribute("list", list);
+
         return "list";
     }
-
 }
