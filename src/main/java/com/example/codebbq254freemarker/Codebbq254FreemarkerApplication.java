b65e0026e0f37befcae15757e2b927da338e2426
Raphael <raphaelndauwa@gmail.com>
2020-07-09 13:11:37 +0300

    add initialization files, also model and controller directories

---
+++ src/main/java/com/example/codebbq254freemarker/Codebbq254FreemarkerApplication.java
@@ -0,0 +1,13 @@
+package com.example.codebbq254freemarker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Codebbq254FreemarkerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Codebbq254FreemarkerApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/example/codebbq254freemarker/model/Student.java
@@ -0,0 +1,7 @@
+package com.example.codebbq254freemarker.model;
+
+public class Student {
+    private Integer Id;
+    private String studentName;
+    private double grade;
+}
